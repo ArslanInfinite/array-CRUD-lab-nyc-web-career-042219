05d188e8381f016de35934bd27442d33184e7c0d
Arslan Ali <ali.arslan319@gmail.com>
2019-04-11 20:05:47 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,35 @@
 def create_an_empty_array
-  
+  starks = []
 end
 
 def create_an_array
-  
+  starks = ["Jon", "Arya", "Sansa", "Bran"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  starks << "Rickon"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  starks.unshift = ["Robb"]
 end
 
 def remove_element_from_end_of_array(array)
-  
+
 end
 
 def remove_element_from_start_of_array(array)
-  
+
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+
 end
 
 def retrieve_first_element_from_array(array)
-  
+
 end
 
 def retrieve_last_element_from_array(array)
-  
+
 end
